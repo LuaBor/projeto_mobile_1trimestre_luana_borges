01acc77397a06e9c581e154b0b86daf9110d9557
Jorgk1ng7 <skyhunger875@gmail.com>
2026-04-08 08:52:19 -0300

    Vitoria (pagina de cadastro) adicionei mais campos de texto e coloquei o botão

--- mobile/app/(tabs)/vit.tsx
+++ mobile/app/(tabs)/vit.tsx
@@ -1,21 +1,30 @@
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet, Text, TextInput } from 'react-native';
+import { Alert, Button, ScrollView, StyleSheet, Text, TextInput } from 'react-native';
 
 export default function HomeScreen() {
   const [texto, setTexto] = useState('');
+  const handleAgendamento = () => {
+    Alert.alert("Sucesso", "Agendamento realizado!");
+  };
   return (
     <ScrollView style={styles.container}>
-      <Text style={styles.label}>Digite seu nome:</Text>
-      <TextInput
-      style={styles.input}
-      
-      
-      
-    
+    <Text style={styles.label}>Nome:</Text>
+    <TextInput style={styles.input} />
+    <Text style={styles.label}>Data:</Text>
+    <TextInput  style={styles.input} ></TextInput>
+    <Text style={styles.label}>Horário de inicio:</Text>
+    <TextInput style={styles.input} />
+    <Text style={styles.label}>Numero de pessoas:</Text>
+    <TextInput style={styles.input} />
+    <Text style={styles.label}></Text>
+    <Button 
+        title="Agendar" 
+        onPress={handleAgendamento} 
+        color="#000" 
       />
-  
     </ScrollView>
-  );
+    
+     );
 }
 
 const styles = StyleSheet.create({
@@ -28,14 +37,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#FFF',
     marginBottom: 8, 
-    marginTop: 50,
+    marginTop: 20,
   },
   input: {
     height: 50,
-    backgroundColor: '#FFF',
+    backgroundColor: '#E6E2E0',
     borderRadius: 8,
     paddingHorizontal: 15,
     fontSize: 16,
     marginTop: 50,
   }
+  
 });
