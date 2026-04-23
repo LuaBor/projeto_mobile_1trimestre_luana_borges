176cd28ae0dfcf5b34288fbc33f5b38613adddbf
Jorgk1ng7 <skyhunger875@gmail.com>
2026-04-23 12:19:04 -0300

    Vitoria (pagina de cadastro) parte da API finalizada

--- mobile/server.js
+++ mobile/server.js
@@ -6,7 +6,7 @@ const usuarios = [
     { id: 2, nome: "Caio Cesar", email: "Caio@email.com" }
 ];
 
-const eventos = [
+let eventos = [
     { id: 1, usuarioid: 1, data: "17-04-2026", horario: "14:00" },
     { id: 2, usuarioid: 2, data: "22-04-2026", horario: "21:00" }
 ];
@@ -30,5 +30,38 @@ app.post('/eventos', (req, res) => {
     };
     eventos.push(novoEvento);
     res.status(201).json(novoEvento); 
+});
+app.put('/eventos/:id', (req, res) => {
+    const { id } = req.params;
+    const { usuarioid, data, horario } = req.body;
+
+    
+    const index = eventos.findIndex(e => e.id === parseInt(id));
+
+    if (index === -1) {
+        return res.status(404).json({ mensagem: "Evento não encontrado" });
+    }
+        eventos[index] = {
+        ...eventos[index],
+        usuarioid: usuarioid || eventos[index].usuarioid,
+        data: data || eventos[index].data,
+        horario: horario || eventos[index].horario
+    };
+    res.json(eventos[index]);
+});
+app.delete('/eventos/:id', (req, res) => {
+    const { id } = req.params;
+
+    
+    const existe = eventos.find(e => String(e.id) === String(id));
+
+    if (!existe) {
+        return res.status(404).json({ mensagem: "Evento não encontrado" });
+    }
+
+   
+    eventos = eventos.filter(e => String(e.id) !== String(id));
+
+    res.json({ mensagem: `Evento ${id} removido com sucesso!` });
 });
 app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000"));
